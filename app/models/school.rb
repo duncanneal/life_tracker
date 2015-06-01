4b8bb4026efc12102b9cea40bbc0222168ffdd2a
Duncan Neal <duncan.neal@gmail.com>
2015-06-01 17:31:13 -0400

    Date?

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,3 @@
 class School < ActiveRecord::Base
-  alidates :end_year, numericality: {greater_than_or_equal_to: :start_year}
+  validates :end_year, numericality: {greater_than_or_equal_to: :start_year}
 end
