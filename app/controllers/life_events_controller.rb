384c0850b5f8dfc14e20c2d7276e275088cf2120
Duncan Neal <duncan.neal@gmail.com>
2015-06-05 14:11:55 -0400

    Working on Welcome Display

--- app/controllers/life_events_controller.rb
+++ app/controllers/life_events_controller.rb
@@ -1,25 +1,20 @@
 class LifeEventsController < ApplicationController
   before_action :set_life_event, only: [:show, :edit, :update, :destroy]
 
-  # GET /life_events
   def index
     @life_events = LifeEvent.all
   end
 
-  # GET /life_events/1
   def show
   end
 
-  # GET /life_events/new
   def new
     @life_event = LifeEvent.new
   end
 
-  # GET /life_events/1/edit
   def edit
   end
 
-  # POST /life_events
   def create
     @life_event = LifeEvent.new(life_event_params)
 
@@ -32,7 +27,7 @@ class LifeEventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /life_events/1
+
   def update
     respond_to do |format|
       if @life_event.update(life_event_params)
@@ -43,7 +38,7 @@ class LifeEventsController < ApplicationController
     end
   end
 
-  # DELETE /life_events/1
+ 
   def destroy
     @life_event.destroy
     respond_to do |format|
@@ -52,13 +47,11 @@ class LifeEventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_life_event
       @life_event = LifeEvent.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def life_event_params
-      params.require(:life_event).permit(:title, :year, :description)
+      params.require(:life_event).permit(:title, :date, :description)
     end
 end
