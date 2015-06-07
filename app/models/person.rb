6d77e61fa12113e895bbc529a4fc20fd3f2ff6c2
Duncan Neal <duncan.neal@gmail.com>
2015-06-07 16:09:08 -0400

    Adding Foundation Stuff

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
   has_many :schools
   has_many :life_events
-
+  accepts_nested_attributes_for :schools, :life_events
   def full_name
     [first_name, last_name].join(' ')
   end
